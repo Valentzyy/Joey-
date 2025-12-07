57eeafd3db1e544b656124831b2a33eb5f8b7599
vava22 <clarizzasetiawan04@gmail.com>
2025-12-07 22:33:49 +0700

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:finalmp/main.dart';
+import 'package:joey/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
